dc60a0659eee0a94cb22b6875ffc751be48fa5d5
Alex <ajoshua14@icloud.com>
2023-06-22 17:17:22 -0700

    Most basic layout

---
+++ app/SideBar.tsx
@@ -0,0 +1,8 @@
+
+const SideBar = () => {
+  return (
+    <div className="w-[17rem] border-2 border-pink-500">SIDE BAR</div>
+  )
+};
+
+export default SideBar;
---
+++ app/TopNav.tsx
@@ -0,0 +1,9 @@
+const TopNav = () => {
+  return (
+    <div className="w-full h-12 fixed border-2 border-orange-500">
+      TOP NAV
+    </div>
+  )
+}
+
+export default TopNav;
